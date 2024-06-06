f35570c16784f81999aeeb078d015832a59ebe05
LimoB <boazlimo07@gmail.com>
2024-06-06 12:31:35 +0300

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Authentication</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4" style="margin-top:20px;">
+                <h4>Welcome To Your Page</h4>
+                <hr>
+                 <table class="table">
+                    <thead>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{$data->name}}</td>
+                            <td>{{$data->email}}</td>
+                            <td><a href="logout">Logout</a></td>
+                        </tr>
+                    </tbody>
+                 </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+<script src="">
+   
+</script>
+</html>
